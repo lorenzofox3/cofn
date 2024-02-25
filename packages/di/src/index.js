38d338ccd5c2227318907513c17f4e2d8265d943
lorenzofox3 <laurent34azerty@gmail.com>
2024-02-25 18:06:36 +0100

    feat(di): provider fn can be an object

--- packages/di/src/index.js
+++ packages/di/src/index.js
@@ -1,14 +1,18 @@
 const rootRegistry = {};
 const registrySymbol = Symbol('registry');
 
-export const provide = (providerFn) => (comp) =>
-  function* ({ $host, ...rest }) {
+const factorify = (factoryLike) =>
+  typeof factoryLike === 'function' ? factoryLike : () => factoryLike;
+
+export const provide = (providerFn) => (comp) => {
+  const _providerFn = factorify(providerFn);
+  return function* ({ $host, ...rest }) {
     yield; // The element must be mounted, so we can look up the DOM tree
     $host[registrySymbol] = Object.assign(
       Object.create(
         $host.closest('[provider]')?.[registrySymbol] ?? rootRegistry,
       ),
-      providerFn({ $host, ...rest }),
+      _providerFn({ $host, ...rest }),
     );
     $host.toggleAttribute('provider');
 
@@ -21,6 +25,7 @@ export const provide = (providerFn) => (comp) =>
 
     yield* instance;
   };
+};
 
 export const inject = (comp) =>
   function* ({ $host, ...rest }) {
@@ -32,9 +37,7 @@ export const inject = (comp) =>
         if (!Reflect.has(services, prop)) {
           throw new Error(`could not resolve injectable "${prop}"`);
         }
-        const factoryLike = services[prop];
-        const factory =
-          typeof factoryLike === 'function' ? factoryLike : () => factoryLike;
+        const factory = factorify(services[prop]);
         return factory(proxy);
       },
     });
